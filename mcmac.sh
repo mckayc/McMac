d55a0f37e10d7844b7f4c9957523d77e2dffd10a
mckayc <mckayjunk@gmail.com>
2019-05-22 13:10:01 -0600

    Update mcmac.sh

--- mcmac.sh
+++ mcmac.sh
@@ -1,2 +1,137 @@
-#This is my new file
+#!/usr/bin/env bash
+# 
+# Bootstrap script for setting up a new OSX machine
+# 
 # Raw https://raw.githubusercontent.com/mckayc/McMac/master/mcmac.sh
+#
+# This should be idempotent so it can be run multiple times.
+#
+# Some apps don't have a cask and so still need to be installed by hand. These
+# include:
+#
+#
+
+echo "Starting bootstrapping"
+
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew tap homebrew/dupes
+brew install coreutils
+brew install gnu-sed --with-default-names
+brew install gnu-tar --with-default-names
+brew install gnu-indent --with-default-names
+brew install gnu-which --with-default-names
+brew install gnu-grep --with-default-names
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+# Install Bash 4
+brew install bash
+
+PACKAGES=(
+    ack
+    autoconf
+    automake
+    boot2docker
+    ffmpeg
+    gettext
+    gifsicle
+    git
+    graphviz
+    hub
+    imagemagick
+    jq
+    libjpeg
+    libmemcached 
+    lynx
+    markdown
+    memcached
+    mercurial
+    npm
+    pkg-config
+    postgresql
+    python
+    python3
+    pypy
+    rabbitmq
+    rename
+    ssh-copy-id
+    terminal-notifier
+    the_silver_searcher
+    tmux
+    tree
+    vim
+    wget
+)
+
+echo "Installing packages..."
+brew install ${PACKAGES[@]}
+
+echo "Cleaning up..."
+brew cleanup
+
+echo "Installing cask..."
+brew install caskroom/cask/brew-cask
+
+CASKS=(
+    darktable
+    gimp
+    inkscape
+    blender
+    audacity
+    tunnelblick
+    obs
+    ultimaker-cura
+    spectacle
+    virtualbox
+    vlc
+)
+
+echo "Installing Google Fonts"
+curl https://raw.githubusercontent.com/qrpike/Web-Font-Load/master/install.sh | sh
+
+#echo "Installing cask apps..."
+#brew cask install ${CASKS[@]}
+#
+#echo "Installing fonts..."
+#brew tap caskroom/fonts
+#FONTS=(
+#    font-inconsolidata
+#    font-roboto
+#    font-clear-sans
+#)
+#brew cask install ${FONTS[@]}
+
+echo "Configuring OSX..."
+
+# Set fast key repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 0
+
+# Require password as soon as screensaver or sleep mode starts
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Show filename extensions by default
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Enable tap-to-click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Disable "natural" scroll
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+echo "Creating folder structure..."
+[[ ! -d Wiki ]] && mkdir Wiki
+[[ ! -d Workspace ]] && mkdir Workspace
+
+echo "Bootstrapping complete"
